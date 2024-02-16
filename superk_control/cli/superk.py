65e462fe23013b9b91ed29ba10d34d840e045c69
vdeo <vdeo@naoj.org>
2024-02-15 17:40:42 -1000

    move logging into cli module

--- superk_control/cli/superk.py
+++ superk_control/cli/superk.py
@@ -10,7 +10,6 @@ Usage:
     superk status
     superk power [on | off]
     superk flux [<value>]
-    superk filter [<pos>]
     superk mode [<mode>]
     superk interlock [reset | disable]
 
@@ -21,55 +20,82 @@ Commands:
     status              Print detailed status message
     power [on | off]    Turn source on or off (case insensitive). If no option provided, returns the power status.
     flux [<value>]      Change the intensity value of the source (0 to 100). If no value provided, return current flux
-    filter [<pos>]      (INACTIVE) Change the ND filter in the source
     mode [<mode>]       Change control mode between normal (0), external trigger (1) or external feedback (2). If no value provided return current mode
     interlock           Return interlock status
               reset     Try and reset interlock
               disable   Disable interlock (DANGEROUS)
-
-    
 """
+UNICODE_SUPPORT = sys.stdout.encoding.lower().startswith("utf")
 
+def flux_bar(flux_value, width=40) -> str:
+    frac = flux_value / 100
+    num_fill = int(frac * width)
+    rest = width - num_fill
+    fill_char = "\u2588" if UNICODE_SUPPORT else "|"
+    return f"0 |{fill_char * num_fill}{'-' * rest}| 100%"
 
-def main():
+def _power_status(superk: SuperK) -> None:
+    status = superk.power_status()
+    if status:
+        print("\x1b[41mSOURCE TURNED ON\x1b[0m")
+    else:
+        print("\x1b[41mSOURCE TURNED OFF\x1b[0m")
+
+def _flux_status(superk: SuperK) -> None:
+    flux = superk.get_flux()
+    print(f"Flux set to: {flux:.01f}%")
+    print(flux_bar(flux))
+
+def _mode_status(superk: SuperK) -> None:
+    value, mode = superk.get_operation_mode()
+    print(f"MODE: {mode} ({value})")
+
+def _interlock_status(superk: SuperK) -> None:
+    isok, code = superk.get_interlock_status()
+    if isok:
+        print(f"\x1b[41mINTERLOCK: {code}\x1b[0m")
+    else:
+        print(f"\x1b[42mINTERLOCK: {code}\x1b[0m")
+
+def main() -> None:
     args = docopt(__doc__)
 
     superk = SuperK()
 
+    # power
     if args["power"]:
         if args["on"]:
             superk.power_on()
         elif args["off"]:
             superk.power_off()
-        else:
-            superk.power_status()
+        _power_status(superk)
+    # flux
     elif args["flux"]:
         if args["<value>"]:
             value = float(args["<value>"])
             superk.set_flux(value)
-        else:
-            superk.get_flux()
-    elif args["filter"]:
-        print(f"No filter options yet")
-        sys.exit(1)
+        _flux_status(superk)
+    # mode
     elif args["mode"]:
         if args["<mode>"]:
             mode = int(args["<mode>"])
             superk.set_operation_mode(mode)
-        else:
-            superk.get_operation_mode()
+        _mode_status(superk)
+    # interlock
     elif args["interlock"]:
         if args["reset"]:
             superk.reset_interlock()
         elif args["disable"]:
             superk.disable_interlock()
-        else:
-            superk.get_interlock_status()
+        _interlock_status(superk)
+    # status
+    elif args["status"]:
+        _interlock_status(superk)
+        _power_status(superk)
+        _mode_status(superk)
+        _flux_status(superk)
     else:
-        superk.get_interlock_status()
-        superk.power_status()
-        superk.get_operation_mode()
-        superk.get_flux()
+        sys.exit(1)
     sys.exit(0)
 
 
--- superk_control/superk.py
+++ superk_control/superk.py
@@ -4,14 +4,9 @@ import os
 
 from .telegram import TelegramInterface
 
-UNICODE_SUPPORT = sys.stdout.encoding.lower().startswith("utf")
-
 DEFAULT_PORT = os.environ.get("SUPERK_PORT", "")
 
-
-class SuperK:
-
-    INTERLOCK_STATUS = {
+INTERLOCK_STATUS = {
         0x0000: "Interlock off (interlock circuit open)",
         0x0001: "Waiting for interlock reset",
         0x0002: "Interlock is OK",
@@ -23,11 +18,12 @@ class SuperK:
         0xFF00: "Interlock circuit failure",
     }
 
-    OPERATION_MODE = {
-        0: "Normal operation",
-        1: "External enable activated",
-        2: "External feedback activated",
-    }
+OPERATION_MODE = {
+    0: "Normal operation",
+    1: "External enable activated",
+    2: "External feedback activated",
+}
+class SuperK:
 
     def __init__(self, port=DEFAULT_PORT, **serial_kwargs):
         self.telegram = TelegramInterface(port, dest=0x0F, **serial_kwargs)
@@ -50,19 +46,19 @@ class SuperK:
         response = self.telegram.read(0x30)
         val = struct.unpack("<B", response[1])[0]
         if val == 0:
-            print("\x1b[41mSOURCE TURNED OFF\x1b[0m")
             return False
         elif val == 2:
-            print("\x1b[42mSOURCE TURNED ON\x1b[0m")
             return True
+        else:
+            msg = f"Unrecognized power status value {val!r}"
+            raise RuntimeError(msg)
 
     def set_flux(self, value):
         if value < 0 or value > 100:
-            raise ValueError(f"Flux value is limited between 0 and 100, got {value}")
-
-        int_value = int(value * 10)
+            msg = f"Flux value is limited between 0 and 100, got {value}"
+            raise ValueError(msg)
         # Output power setpoint 27h 0-1000 per-thousands in 16-bit unsigned int
-        msg = struct.pack("<H", int_value)
+        msg = struct.pack("<H", int(value * 10))
         self.telegram.write(0x27, msg)
         self.get_flux()
 
@@ -70,8 +66,6 @@ class SuperK:
         response = self.telegram.read(0x27)
         int_value = struct.unpack("<H", response[1])[0]
         value = int_value / 10
-        print(f"Flux set to: {value}")
-        print(flux_bar(value))
         return value
 
     def reset_interlock(self):
@@ -88,12 +82,8 @@ class SuperK:
         response = self.telegram.read(0x32)
         msb = response[1][1]
         value = struct.unpack("<H", response[1])[0]
-        code = self.INTERLOCK_STATUS[value]
-        if msb == 0:
-            print(f"\x1b[42mINTERLOCK: {code}\x1b[0m")
-        else:
-            print(f"\x1b[41mINTERLOCK: {code}\x1b[0m")
-        return msb == 0
+        code = INTERLOCK_STATUS[value]
+        return msb, code
 
     def set_operation_mode(self, mode: int):
         if mode < 0 or mode > 2:
@@ -105,18 +95,10 @@ class SuperK:
     def get_operation_mode(self):
         response = self.telegram.read(0x31)
         value = struct.unpack("<B", response[1])[0]
-        code = self.OPERATION_MODE[value]
-        print(f"MODE: {code}")
-        return value
+        code = OPERATION_MODE[value]
+        return value, code
 
     def get_status_bits(self):
         response = self.telegram.read(0x66)
         return response[1]
 
-
-def flux_bar(flux_value, width=40):
-    frac = flux_value / 100
-    num_fill = int(frac * width)
-    rest = width - num_fill
-    fill_char = "\u2588" if UNICODE_SUPPORT else "|"
-    return f"{0} {fill_char * num_fill}{'-' * rest} {100}"
